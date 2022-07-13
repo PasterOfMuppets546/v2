ddd6c304f60d965b373b73764a43733a49042616
RyanJEarl546 <109228098+RyanJEarl546@users.noreply.github.com>
2022-07-13 09:22:03 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace look_and_listened
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 11)
+            {
+            for(int x = 0; x<i; x++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("Hello, World");
+                i++;
+
+            }
+        }
+    }
+}
